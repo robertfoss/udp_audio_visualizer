edc48b8bb3f39d996eb9b5d047879af04256ef4f
Robert Foss <dev@robertfoss.se>
2016-01-23 22:45:19 -0500

    Made sure that the animation loops doesn't run continously.

--- main.c
+++ main.c
@@ -12,12 +12,17 @@ int main()
     udp_listener_start();
     audio_process_start();
 
+    struct timespec time_sleep;
+    time_sleep.tv_sec = 0;
+    time_sleep.tv_nsec = 5*10*1000*1000;
+
     while (1) {
         sink_manager_list_t *list = sink_manager_get_list();
         for (int i = 0; i < list->nbr_sinks; i++) {
             unsigned char txt[] = "0A0A0A0A0A0A0A0A0A";
             udp_sender_send(list->sinks[i], (uint8_t *) &txt);
         }
+        nanosleep(&time_sleep, NULL);
         sink_manager_free_list(list);
     }
     debug();
